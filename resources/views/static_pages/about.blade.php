c3dec19386ec9baa0fbd8b4c8e4fb7e01e321704
yxs <707914799@qq.com>
2019-07-20 10:14:16 +0800

    基础页面

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+
+@section('title','关于')
+
+@section('content')
+
+@stop
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+
+@section('title','帮助')
+
+@section('content')
+
+@stop
